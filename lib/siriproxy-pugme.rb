963da7827810b5a66fc44342df6edd6c88c57d27
elvisimprsntr <casual@alecdawson.com>
2012-10-22 17:37:08 -0300

    Update lib/siriproxy-pugme.rb

--- lib/siriproxy-pugme.rb
+++ lib/siriproxy-pugme.rb
@@ -1,23 +1,9 @@
 require 'httparty'
 
-class SiriProxy::Plugin::Pugme < SiriProxy::Plugin
+class SiriProxy::Plugin::XXX < SiriProxy::Plugin
   def initialize(config = {})
   end
 
-  listen_for /pug me/i do
-      pugUrl = HTTParty.get("http://pugme.herokuapp.com/random").parsed_response['pug']
+# under construction
 
-    say "Here is your pug:"
-
-    # send a "Preview" of the Tweet
-    object = SiriAddViews.new
-    object.make_root(last_ref_id)
-    answer = SiriAnswer.new("Pugme", [
-      SiriAnswerLine.new('logo',pugUrl)
-    ])
-    object.views << SiriAnswerSnippet.new([answer])
-    send_object object
-
-    request_completed
-  end
 end
