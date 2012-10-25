6acc4bb3e27c6f6ef70a059d6d8d6865ad1dfbdb
elvisimprsntr <casual@alecdawson.com>
2012-10-25 06:39:21 -0300

    Update lib/siriproxy-ipcam.rb

--- lib/siriproxy-ipcam.rb
+++ lib/siriproxy-ipcam.rb
@@ -2,20 +2,20 @@ require 'siri_objects'
 require 'cgi'
 
 class SiriProxy::Plugin::IPCam < SiriProxy::Plugin
+  attr_accessor :camid
+  attr_accessor :campw
   attr_accessor :camurl1
   attr_accessor :camurl2
   attr_accessor :camurl3
   attr_accessor :camurl4
-  attr_accessor :camid
-  attr_accessor :campw
-
+  
   def initialize(config)
+    self.camid = config["camid"]
+    self.campw = config["campw"]
     self.camurl1 = config["camurl1"]
     self.camurl2 = config["camurl2"]
     self.camurl3 = config["camurl3"]
     self.camurl4 = config["camurl4"]
-    self.camid = config["camid"]
-    self.campw = config["campw"]
 
 
 end
