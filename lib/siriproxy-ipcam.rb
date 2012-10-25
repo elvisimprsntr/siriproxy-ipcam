a86cf942747f7d948b5519cd4f63287b74a95a0f
elvisimprsntr <casual@alecdawson.com>
2012-10-24 21:34:43 -0300

    Update lib/siriproxy-ipcam.rb

--- lib/siriproxy-ipcam.rb
+++ lib/siriproxy-ipcam.rb
@@ -3,9 +3,15 @@ require 'cgi'
 
 class SiriProxy::Plugin::IPCam < SiriProxy::Plugin
   attr_accessor :camip1
+  attr_accessor :camip2
+  attr_accessor :camip3
+  attr_accessor :camip4
 
   def initialize(config)
     self.camip1 = config["camip1"]
+    self.camip2 = config["camip2"]
+    self.camip3 = config["camip3"]
+    self.camip4 = config["camip4"]
 
 end
 
