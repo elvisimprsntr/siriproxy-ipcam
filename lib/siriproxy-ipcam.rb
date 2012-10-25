90c7aea8babacf4ac35c9e04963dd81d9a70244c
elvisimprsntr <casual@alecdawson.com>
2012-10-25 08:02:50 -0300

    testing complete

--- lib/siriproxy-ipcam.rb
+++ lib/siriproxy-ipcam.rb
@@ -27,70 +27,66 @@ listen_for(/attic cam/i) {attic_cam}
 
 listen_for(/living cam/i) {living_cam}
 
-listen_for(/check cameras/i) {camera_check)  
+listen_for(/check cameras/i) {check_cameras}  
 
 
   def garage_cam
-  # turn on light
-	Rest.get("#{self.isyip}/rest/nodes/27356/cmd/DON", :basic_auth => @isyauth)
-	# push garage camera image to phone	
+	# push camera image to phone	
 	object = SiriAddViews.new
 	object.make_root(last_ref_id)
-	answer = SiriAnswer.new("Garage Camera", [SiriAnswerLine.new('logo',"#{self.camurl1")])
+	answer = SiriAnswer.new("Garage Camera", [SiriAnswerLine.new('logo',"#{self.camurl1}")])
 	object.views << SiriAnswerSnippet.new([answer])
 	send_object object
-	# turn off light
-	Rest.get("#{self.isyip}/rest/nodes/27356/cmd/DOF", :basic_auth => @isyauth)
     request_completed
   end
 
   def porch_cam
-	# turn on light
-	Rest.get("#{self.isyip}/rest/nodes/32377/cmd/DON", :basic_auth => @isyauth)
-	# push garage camera image to phone	
+	# push camera image to phone	
 	object = SiriAddViews.new
 	object.make_root(last_ref_id)
-	answer = SiriAnswer.new("Porch Camera", [SiriAnswerLine.new('logo',"#{self.camurl2")])
+	answer = SiriAnswer.new("Porch Camera", [SiriAnswerLine.new('logo',"#{self.camurl2}")])
 	object.views << SiriAnswerSnippet.new([answer])
 	send_object object
-	# turn off light
-	Rest.get("#{self.isyip}/rest/nodes/27356/cmd/DOF", :basic_auth => @isyauth)
     request_completed
   end
 
   def attic_cam
-	# turn on light
-	Rest.get("#{self.isyip}/rest/nodes/18595/cmd/DON", :basic_auth => @isyauth)
-	# push garage camera image to phone	
+	# push camera image to phone	
 	object = SiriAddViews.new
 	object.make_root(last_ref_id)
-	answer = SiriAnswer.new("Attic Camera", [SiriAnswerLine.new('logo',"#{self.camurl3")])
+	answer = SiriAnswer.new("Attic Camera", [SiriAnswerLine.new('logo',"#{self.camurl3}")])
 	object.views << SiriAnswerSnippet.new([answer])
 	send_object object
-	# turn off light
-	Rest.get("#{self.isyip}/rest/nodes/27356/cmd/DOF", :basic_auth => @isyauth)
     request_completed
   end
 
   def living_cam
-	# turn on light
-	Rest.get("#{self.isyip}/rest/nodes/19496/cmd/DON", :basic_auth => @isyauth)
-	# push garage camera image to phone	
+	# push camera image to phone	
 	object = SiriAddViews.new
 	object.make_root(last_ref_id)
-	answer = SiriAnswer.new("Living Camera", [SiriAnswerLine.new('logo',"#{self.camurl4")])
+	answer = SiriAnswer.new("Living Camera", [SiriAnswerLine.new('logo',"#{self.camurl4}")])
 	object.views << SiriAnswerSnippet.new([answer])
 	send_object object
-	# turn off light
-	Rest.get("#{self.isyip}/rest/nodes/27356/cmd/DOF", :basic_auth => @isyauth)
     request_completed
   end
 
-  def camera_check
-	garage_cam
-	porch_cam
-	attic_cam
-	living_cam
+  def check_cameras
+	object = SiriAddViews.new
+	object.make_root(last_ref_id)
+
+	answer = SiriAnswer.new("Garage Camera", [SiriAnswerLine.new('logo',"#{self.camurl1}")])
+	object.views << SiriAnswerSnippet.new([answer])
+
+	answer = SiriAnswer.new("Porch Camera", [SiriAnswerLine.new('logo',"#{self.camurl2}")])
+	object.views << SiriAnswerSnippet.new([answer])
+
+	answer = SiriAnswer.new("Attic Camera", [SiriAnswerLine.new('logo',"#{self.camurl3}")])
+	object.views << SiriAnswerSnippet.new([answer])
+
+	answer = SiriAnswer.new("Living Camera", [SiriAnswerLine.new('logo',"#{self.camurl4}")])
+	object.views << SiriAnswerSnippet.new([answer])
+
+	send_object object
   end
 	
 end
