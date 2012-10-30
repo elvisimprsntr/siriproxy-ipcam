96d93c4364bbec5d8df575907a694600b4edb6c1
elvisimprsntr <casual@alecdawson.com>
2012-10-29 21:33:27 -0300

    Update lib/siriproxy-ipcam.rb

--- lib/siriproxy-ipcam.rb
+++ lib/siriproxy-ipcam.rb
@@ -17,77 +17,32 @@ class SiriProxy::Plugin::IPCam < SiriProxy::Plugin
     self.camurl3 = config["camurl3"]
     self.camurl4 = config["camurl4"]
     @camauth = {:username => "#{self.camid}", :password => "#{self.campw}"}
+
+@camUrl = Hash.new
+@camUrl["garage"] = "#{self.camurl1}"
+@camUrl["porch"] = "#{self.camurl2}"
+@camUrl["attic"] = "#{self.camurl3}"
+@camUrl["addict"] = "#{self.camurl3}"
+@camUrl["living room"] = "#{self.camurl4}"
+
   end
 
-listen_for(/garage cam/i) {garage_cam}
-
-listen_for(/porch cam/i) {porch_cam}
-
-listen_for(/attic cam/i) {attic_cam}
-
-listen_for(/living cam/i) {living_cam}
-
-listen_for(/check cameras/i) {check_cameras}  
-
-
-  def garage_cam
-	# push camera image to phone	
-	object = SiriAddViews.new
-	object.make_root(last_ref_id)
-	answer = SiriAnswer.new("Garage Camera", [SiriAnswerLine.new('logo',"#{self.camurl1}")])
-	object.views << SiriAnswerSnippet.new([answer])
-	send_object object
-    request_completed
+  listen_for(/camera (.*)/i) do |camera|
+	check_camera camera
   end
-
-  def porch_cam
-	# push camera image to phone	
-	object = SiriAddViews.new
-	object.make_root(last_ref_id)
-	answer = SiriAnswer.new("Porch Camera", [SiriAnswerLine.new('logo',"#{self.camurl2}")])
-	object.views << SiriAnswerSnippet.new([answer])
-	send_object object
-    request_completed
+  
+  def check_camera(camera)
+	cameraurl = @camUrl[camera.downcase.strip]
+	unless cameraurl.nil?
+		object = SiriAddViews.new
+		object.make_root(last_ref_id)
+		answer = SiriAnswer.new("#{camera}",[SiriAnswerLine.new('logo',"#{cameraurl}")])
+		object.views << SiriAnswerSnippet.new([answer])
+		send_object object
+	else
+		say "Sorry, I could not find a camera named #{camera}."
+	end
+     request_completed
   end
-
-  def attic_cam
-	# push camera image to phone	
-	object = SiriAddViews.new
-	object.make_root(last_ref_id)
-	answer = SiriAnswer.new("Attic Camera", [SiriAnswerLine.new('logo',"#{self.camurl3}")])
-	object.views << SiriAnswerSnippet.new([answer])
-	send_object object
-    request_completed
-  end
-
-  def living_cam
-	# push camera image to phone	
-	object = SiriAddViews.new
-	object.make_root(last_ref_id)
-	answer = SiriAnswer.new("Living Camera", [SiriAnswerLine.new('logo',"#{self.camurl4}")])
-	object.views << SiriAnswerSnippet.new([answer])
-	send_object object
-    request_completed
-  end
-
-  def check_cameras
-	object = SiriAddViews.new
-	object.make_root(last_ref_id)
-
-	answer = SiriAnswer.new("Garage Camera", [SiriAnswerLine.new('logo',"#{self.camurl1}")])
-	object.views << SiriAnswerSnippet.new([answer])
-
-	answer = SiriAnswer.new("Porch Camera", [SiriAnswerLine.new('logo',"#{self.camurl2}")])
-	object.views << SiriAnswerSnippet.new([answer])
-
-	answer = SiriAnswer.new("Attic Camera", [SiriAnswerLine.new('logo',"#{self.camurl3}")])
-	object.views << SiriAnswerSnippet.new([answer])
-
-	answer = SiriAnswer.new("Living Camera", [SiriAnswerLine.new('logo',"#{self.camurl4}")])
-	object.views << SiriAnswerSnippet.new([answer])
-
-	send_object object
-    request_completed
-end
 	
 end
