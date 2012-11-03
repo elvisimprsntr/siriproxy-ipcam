63ae1297d40e2ff51ab4b24361a71151bdc642ae
Elvis <casual@alecdawson.com>
2012-11-03 17:56:45 -0300

    pass key/value hash for camera URLs

--- lib/siriproxy-ipcam.rb
+++ lib/siriproxy-ipcam.rb
@@ -5,35 +5,24 @@ class SiriProxy::Plugin::IPCam < SiriProxy::Plugin
   attr_accessor :camid
   attr_accessor :campw
   attr_accessor :webip
-  attr_accessor :camurl1
-  attr_accessor :camurl2
-  attr_accessor :camurl3
-  attr_accessor :camurl4
+  attr_accessor :camurls
   
-  def initialize(config)
-    @camAuth = Hash.new
-    @camAuth["porch"] = {:http_basic_authentication => [config["camid"], config["campw"]]}
+  def initialize(config = {})
+    @camAuth = {:http_basic_authentication => [config["camid"], config["campw"]]} if config["camid"] 
     @webIp = config["webip"] 
     @camUrl = Hash.new
-    @camUrl["garage"] = config["camurl1"]
-    @camUrl["porch"] = config["camurl2"]
-    @camUrl["attic"] = config["camurl3"]
-    @camUrl["addict"] = config["camurl3"]
-    @camUrl["living room"] = config["camurl4"]
+    @camUrl = config["camurls"]
   end
-
 
   listen_for(/camera (.*)/i) do |camera|
 	check_camera camera
   end
 
-
   def check_camera(camera)
 	cameraurl = @camUrl[camera.downcase.strip]
-	camauth = @camAuth[camera.downcase.strip]
 	unless cameraurl.nil?
-		unless camauth.nil?
-			open(cameraurl, camauth) do |f|
+		unless @camAuth.nil?
+			open(cameraurl, @camAuth) do |f|
 				File.open("/var/www/image.jpg","wb") do |file|
 					file.puts f.read
   				end
