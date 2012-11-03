89664ee29519698656e61503a96499551bbabf12
Elvis <casual@alecdawson.com>
2012-11-03 12:31:23 -0300

    added support for authentication

--- lib/siriproxy-ipcam.rb
+++ lib/siriproxy-ipcam.rb
@@ -1,3 +1,4 @@
+require 'open-uri'
 require 'siri_objects'
 
 class SiriProxy::Plugin::IPCam < SiriProxy::Plugin
@@ -7,24 +8,38 @@ class SiriProxy::Plugin::IPCam < SiriProxy::Plugin
   attr_accessor :camurl2
   attr_accessor :camurl3
   attr_accessor :camurl4
+  attr_accessor :webip
   
   def initialize(config)
-    @camauth = {:username => config["camid"], :password => config["campw"]}
+    @camAuth = Hash.new
+    @camAuth["porch"] = {:http_basic_authentication => [config["camid"], config["campw"]]}
     @camUrl = Hash.new
     @camUrl["garage"] = config["camurl1"]
     @camUrl["porch"] = config["camurl2"]
     @camUrl["attic"] = config["camurl3"]
     @camUrl["addict"] = config["camurl3"]
     @camUrl["living room"] = config["camurl4"]
+    @webIp = config["webip"] 
   end
+
 
   listen_for(/camera (.*)/i) do |camera|
 	check_camera camera
   end
-  
+
+
   def check_camera(camera)
 	cameraurl = @camUrl[camera.downcase.strip]
+	camauth = @camAuth[camera.downcase.strip]
 	unless cameraurl.nil?
+		unless camauth.nil?
+			open(cameraurl, camauth) do |f|
+				File.open("/var/www/image.jpg","wb") do |file|
+					file.puts f.read
+  				end
+			end
+			cameraurl = "#{@webIp}/image.jpg"
+		end	
 		object = SiriAddViews.new
 		object.make_root(last_ref_id)
 		answer = SiriAnswer.new("#{camera}",[SiriAnswerLine.new('logo',"#{cameraurl}")])
