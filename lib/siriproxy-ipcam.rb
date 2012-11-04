73731d4f5d91de5eb6f57126511f9ce5071ba6ee
Elvis <casual@alecdawson.com>
2012-11-04 03:10:09 -0500

    remove unnecessary {}

--- lib/siriproxy-ipcam.rb
+++ lib/siriproxy-ipcam.rb
@@ -2,17 +2,17 @@ require 'open-uri'
 require 'siri_objects'
 
 class SiriProxy::Plugin::IPCam < SiriProxy::Plugin
-  attr_accessor :camurls
   attr_accessor :camid
   attr_accessor :campw
   attr_accessor :webip
+  attr_accessor :camurls
   
   def initialize(config = {})
-    @camUrl = Hash.new
-    @camUrl = config["camurls"]
     @camAuth = nil
     @camAuth = {:http_basic_authentication => [config["camid"], config["campw"]]} if config["camid"] 
     @webIp = config["webip"] 
+    @camUrl = Hash.new
+    @camUrl = config["camurls"]
   end
 
   listen_for(/camera (.*)/i) do |camera|
@@ -32,7 +32,7 @@ class SiriProxy::Plugin::IPCam < SiriProxy::Plugin
 		end	
 		object = SiriAddViews.new
 		object.make_root(last_ref_id)
-		answer = SiriAnswer.new("#{camera}",[SiriAnswerLine.new('logo',"#{cameraurl}")])
+		answer = SiriAnswer.new(camera,[SiriAnswerLine.new('logo',cameraurl)])
 		object.views << SiriAnswerSnippet.new([answer])
 		send_object object
 	else
