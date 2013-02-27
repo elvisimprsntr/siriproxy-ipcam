d7e49e58ec72cf465aefa0957974cac3d9505d3e
elvisimprsntr <elvisimprsntr@gmail.com>
2013-02-27 12:15:52 -0500

    Enhancement #4

--- lib/siriproxy-ipcam.rb
+++ lib/siriproxy-ipcam.rb
@@ -13,7 +13,7 @@ class SiriProxy::Plugin::IPCam < SiriProxy::Plugin
     @camAuth = nil
     @camAuth = {:http_basic_authentication => [config["camid"], config["campw"]]} if config["camid"] 
     @webIp = "http://" + UDPSocket.open {|s| s.connect("255.255.255.0", 1); s.addr.last}
-  end
+  end 
   
 ########## Commands   
 
